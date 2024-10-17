30057ecbdf8328470309094b67df6a51883c9e03
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2024-10-17 22:36:30 +0000

    Successfully Generated report and built the app

--- src/components/ViabilityPage.tsx
+++ src/components/ViabilityPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SimpleCard } from "./SimpleCard.js";
 import { SketchAttributesCard } from "@seasketch/geoprocessing/client-ui";
+import { BoundaryAreaOverlap } from "./BoundaryAreaOverlap.tsx";
 
 export const ViabilityPage = () => {
   return (
     <>
       <SimpleCard />
+      <BoundaryAreaOverlap />
       <SketchAttributesCard autoHide />
     </>
   );
